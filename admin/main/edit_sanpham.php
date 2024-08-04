b3598ed448c838d33dc0eab7f6509add48b2fb62
Huyn01 <huyenntph42110@fpt.edu.vn>
2024-08-04 22:14:09 +0700

    edit_sanpham

--- admin/main/edit_sanpham.php
+++ admin/main/edit_sanpham.php
@@ -13,3 +13,49 @@
             </div>
         </div>
     </div>
+    
+    <!-- Basic Information Section -->
+    <div class="row">
+        <div class="col-xxl-12">
+            <div class="row">
+                <div class="col-lg-8">
+                    <div class="card mb-8 rounded-4">
+                        <div class="card-header p-7 bg-transparent">
+                            <h4 class="fs-18 mb-0 font-weight-500">Basic</h4>
+                        </div>
+                        <div class="card-body p-7">
+                            <div class="mb-8">
+                                <label for="product_title" class="mb-4 fs-13px ls-1 fw-bold text-uppercase">Product
+                                    Title</label>
+                                <input type="hidden" name="id" value="<?=$in4['id']?>">
+                                <input type="text" placeholder="Type here" class="form-control" id="product_title"
+                                    name="name" value="<?= $in4['name'] ?>">
+                            </div>
+                            <div class="mb-8">
+                                <label class="mb-4 fs-13px ls-1 fw-bold text-uppercase">Full Description</label>
+                                <textarea placeholder="Type here" class="form-control" rows="4"
+                                    name="fact"><?= $in4['fact'] ?></textarea>
+                            </div>
+                            <div class="row">
+                                <div class="col-lg-4 mb-8">
+                                    <label class="mb-4 fs-13px ls-1 fw-bold text-uppercase" for="16cm">Price Size
+                                        16cm</label>
+                                    <input placeholder="Enter price..." type="number" class="form-control" id="16cm"
+                                        name="size_nho" value="<?= $p_size[0]['price'] ?>">
+                                </div>
+                                <div class="col-lg-4 mb-8">
+                                    <label class="mb-4 fs-13px ls-1 fw-bold text-uppercase" for="20cm">Price Size
+                                        20cm</label>
+                                    <input placeholder="Enter price..." type="number" class="form-control" id="20cm"
+                                        name="size_vua" value="<?= $p_size[1]['price'] ?>">
+                                </div>
+                                <div class="col-lg-4 mb-8">
+                                    <label class="mb-4 fs-13px ls-1 fw-bold text-uppercase" for="24cm">Price Size
+                                        24cm</label>
+                                    <input placeholder="Enter price..." type="number" class="form-control" id="24cm"
+                                        name="size_lon" value="<?= $p_size[2]['price'] ?>">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
