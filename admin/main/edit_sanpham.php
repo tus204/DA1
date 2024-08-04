8b58777bc643d214e4557b68d79f43ce172a9317
Huyn01 <huyenntph42110@fpt.edu.vn>
2024-08-04 22:12:43 +0700

    edit_sanpham

---
+++ admin/main/edit_sanpham.php
@@ -0,0 +1,15 @@
+<form action="?act=edit_sanpham&id=<?= $pro_id ?>" method="POST" enctype="multipart/form-data">
+    <!-- Header Section -->
+    <div class="row mb-9 align-items-center">
+        <div class="col-xxl-12">
+            <div class="row">
+                <div class="col-sm-6 mb-8 mb-sm-0">
+                    <h2 class="fs-4 mb-0">Edit Product</h2>
+                </div>
+                <div class="col-sm-6 d-flex flex-wrap justify-content-sm-end">
+                    <a href="#" class="btn btn-outline-primary me-4">Save to draft</a>
+                    <button type="submit" name="submit" class="btn btn-primary">Update</button>
+                </div>
+            </div>
+        </div>
+    </div>
