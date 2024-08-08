b416348c3cec9241b77f92c5cba50e5a24118ee7
linh <linhntph43230@fpt.edu.vn>
2024-08-08 22:45:37 +0700

    update-contact

---
+++ user/contact.php
@@ -0,0 +1,64 @@
+<!-- Breadcrumb Begin -->
+<div class="breadcrumb-option">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="breadcrumb__links">
+                    <a href="./index.html"><i class="fa fa-home"></i> Home</a>
+                    <span>Contact</span>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- Breadcrumb End -->
+
+<!-- Contact Section Begin -->
+<section class="contact spad">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-6 col-md-6">
+                <div class="contact__content">
+                    <div class="contact__address">
+                        <h5>THÔNG TIN LIÊN LẠC</h5>
+                        <ul>
+                            <li>
+                                <h6><i class="fa fa-map-marker"></i> Địa chỉ</h6>
+                                <p>2PQV+J76, P. Trịnh Văn Bô, Hòe Thị, Nam Từ Liêm, Hà Nội, Vietnam</p>
+                            </li>
+                            <li>
+                                <h6><i class="fa fa-phone"></i> Sô điện thoại</h6>
+                                <p><span>0974566306</span><span>0123456789</span></p>
+                            </li>
+                            <li>
+                                <h6><i class="fa fa-headphones"></i> Hỗ trợ</h6>
+                                <p>Support.tus@gmail.com</p>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="contact__form">
+                        <h5>
+                            GỬI TIN NHẮN</h5>
+                        <form action="#">
+                            <input type="text" placeholder="Họ tên">
+                            <input type="text" placeholder="Email">
+                            <input type="text" placeholder="Website">
+                            <textarea placeholder="Tin nhắn"></textarea>
+                            <button type="submit" class="site-btn">GỬI TIN NHẮN</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-6 col-md-6">
+                <div class="contact__map">
+                    <iframe
+                        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3723.8409070250245!2d105.74057277471464!3d21.039050787426078!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3134550027a0d927%3A0xfff73c5781923fc2!2zVHLhu4tuaCB2xINuIGLDtA!5e0!3m2!1sen!2s!4v1722999407997!5m2!1sen!2s"
+                        width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"
+                        referrerpolicy="no-referrer-when-downgrade"></iframe>
+
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- Contact Section End -->
