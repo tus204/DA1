ab2f4fefab2c302a8829df939b0969b9f004bcb8
Huyn01 <huyenntph42110@fpt.edu.vn>
2024-08-08 16:48:27 +0700

    orderdetail

--- views/my-order.php
+++ views/my-order.php
@@ -35,3 +35,90 @@
 
         $date_formated = $BaseModel->date_format($date, '');
 ?>
+<div class="container pt-4 mb-0">
+    <article class="card">
+        <div class="card-header" style="background-color: #f9f9f9">
+            <span class="fw-500 text-black">
+                Trạng thái:
+                <span style="font-weight: 600;" class="text-danger"><?=$order_status?></span> 
+            </span>
+            <span class="float-right text-black">
+                Thời gian: 
+                <span style="font-weight: 600;" class="text-primary"><?=$date_formated?></span> 
+            </span>
+        </div>
+
+        <div class="card-body">
+
+            <ul class="row">
+                
+                
+                <?php
+                    foreach ($list_products_buyed as $value) {
+                        extract($value);
+                ?>
+                <li class="col-md-4">
+                    <figure class="itemside mb-3">
+                        <div class="aside"><img src="upload/<?=$image?>" class="img-sm border"></div>
+                        <figcaption class="info align-self-center">
+                            <p class="title"><?=$product_name?> </p> 
+                            <span class="text-primary"><?=number_format($product_price)?>₫</span> <span style="font-size: 16px;" class="text-dark" >x<?=$quantity?></span>
+                        </figcaption>
+                    </figure>
+                </li>
+                <?php
+                   }
+                ?>
+                
+                
+            </ul>
+            <hr>
+            <div >
+                <a href="#" class="btn btn-custom" data-abc="true"> <i class="fa fa-chevron-left"></i> Trở lại</a>
+                <div class="float-right">
+                    <span class="text-dark">Thành tiền: </span> 
+                    <span style="font-weight: 600;" class="text-danger mr-3"><?=number_format($total)?>₫</span>
+                    <a href="index.php?url=chi-tiet-don-hang&id=<?=$order_id?>" class="btn btn-custom"> Xem chi tiết</a>
+                </div>
+            </div>
+        </div>
+    </article>
+</div>
+<?php
+    }
+?>
+
+<?php
+    } else {
+?>
+<div class="row" style="margin-bottom: 400px;">
+    <div class="col-lg-12 col-md-12">
+        <div class="container-fluid mt-5">
+            <div class="row rounded justify-content-center mx-0 pt-5">
+                <div class="col-md-6 text-center">
+                    <h4 class="mb-4">Vui lòng đăng nhập để có thể sử dụng chức năng</h4>
+                    <a class="btn btn-primary rounded-pill py-3 px-5" href="index.php?url=dang-nhap">Đăng nhập</a>
+                    <a class="btn btn-secondary rounded-pill py-3 px-5" href="index.php">Trang chủ</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<?php
+    }
+?>
+
+<div style="margin-bottom: 200px;"></div>
+<style>
+    .btn-custom {
+        color: #555;
+        background-color: #f6f6f6;
+        border-color: rgba(0,0,0,.09);
+    }
+
+    .btn-custom:hover {
+        background-color: #fff;
+    }
+
+    
+</style>
